d359452b007b96ca12b672841c9f66227b1eaa3f
ADMIN <ADMIN@192.168.0.4>
2017-06-08 11:05:19 +0530

    Web App Code for BasicCalc.java

--- src/calculator/BasicCalc.java
+++ src/calculator/BasicCalc.java
@@ -1,12 +1,48 @@
 package calculator;
-
-
 public class BasicCalc
 {
-	// include variables a, b, c here
-	 
-	//include getters and setters method to access variables
 	
-	//include methods for operations
+
+	double a,b,c;// include variables a, b, c here
+	public double getA() //include getters and setters method to access variables
 	
+	{
+		return a;
+	}
+	public void setA(double a)
+	{
+		this.a = a;
+	}
+	public double getB() 
+	{
+		return b;
+	}
+	public void setB(double b) 
+	{
+		this.b = b;
+	}
+	double add()//include methods for operations 
+	{
+		c=a+b;
+		return(c);
+	}
+	double substract()
+	{
+		c=a-b;
+		return(c);
+	}
+	double multiply()
+	{
+		c=a*b;
+		return(c);
+	}
+	double divide()
+	{
+		c=a/b;
+		return(c);
+	}
 }
+
+	
+	
+
