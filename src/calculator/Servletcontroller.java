af2e1261384fb4d59320b3b449a3554188dcd1ba
ADMIN <ADMIN@192.168.0.4>
2017-06-08 11:06:48 +0530

    Web App code for Servletcontroller.java

--- src/calculator/Servletcontroller.java
+++ src/calculator/Servletcontroller.java
@@ -2,7 +2,6 @@ package calculator;
 
 import java.io.IOException;
 
-
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -14,30 +13,44 @@ import javax.servlet.http.HttpSession;
 
 @WebServlet("/ServletController")
 
-
-
 	public class Servletcontroller extends HttpServlet {
 
 	private static final long serialVersionUID = 1L;
 	 double c;
-
-
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
 	throws ServletException, IOException 
 	{
-		// Create an object of BasicCalc class
+		BasicCalc det =new BasicCalc();
+		double a=Double.valueOf(req.getParameter("a"));
+		double b=Double.valueOf(req.getParameter("b"));
+		det.setA(a);
+		det.setB(b);
+		String operation=req.getParameter("operation");
+	
+			switch(operation)
+			{
+				case "Add":
+					c=det.add();
+					System.out.println("c"+c);
+					break;
+				case "Subtract":
+					c=det.substract();
+					System.out.println("c");
+					break;
+				case "Multiply":
+					c=det.multiply();
+					System.out.println("c");
+					break;
+				case "Divide":
+					c=det.divide();
+					System.out.println("c");
+					break;
+					default:System.out.println("wrong choice");
+					break;
+			}	
+			req.setAttribute("answer",c);
 		
-		//get parameter with req.getparameter() method and 
-		
-		// set the values with set parameter into variable a, b
-		
-		// get parameter operation 
-		
-	//	switch (operation)		
-		{
-		//write switch cases for calling different method of operations
-		}
 		RequestDispatcher rd = req.getRequestDispatcher("index.jsp");
 		rd.forward(req, resp); 
 		} 
